11a99f3192e93f3398a2e03b2ddeffdd975e9cda
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2024-07-07 12:36:28 -0300

    Configurações Django com Jwt

---
+++ user/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics, serializers, permissions
+from user.models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (permissions.IsAuthenticated,)
